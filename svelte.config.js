57fe7e3ac97b551d49f0e1ded6ff065a73a202df
TMNT <trietpersonal@gmail.com>
2022-05-13 12:28:24 -0400

    dynamic basePath via env var

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+// dynamic basePath set via env var SVELTE_APP_BASEPATH
+const basePath = "/" + process.env.SVELTE_APP_BASEPATH.replaceAll("/", "")
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    files: {
+      lib: "src/lib"
+    },
+    paths: {
+      base: basePath
+    }
+  }
 };
 
 export default config;
